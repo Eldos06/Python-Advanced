8b2adc87fb9b7b4675742875e229c80c04321b59
YeldoS2 <eldossuleimen2006@gmail.com>
2025-07-16 02:15:50 +0500

    Fix YeldoS2

--- 6.asynchronous/practice/lecture/2/client.py
+++ 6.asynchronous/practice/lecture/2/client.py
@@ -1,16 +1,23 @@
 import asyncio
 
-async def tcp_client():
-    reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
-    message = 'Hello, server!'
-    writer.write(message.encode())
-    await writer.drain()
 
-    data = await reader.read(100)
-    print(f'Received: {data.decode()}')
 
-    writer.close()
-    await writer.wait_closed()
 
-if __name__ == '__main__':
-    asyncio.run(tcp_client())
+
+
+
+
+# async def tcp_client():
+#     reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
+#     message = 'Hello, server!'
+#     writer.write(message.encode())
+#     await writer.drain()
+
+#     data = await reader.read(100)
+#     print(f'Received: {data.decode()}')
+
+#     writer.close()
+#     await writer.wait_closed()
+
+# if __name__ == '__main__':
+#     asyncio.run(tcp_client())
